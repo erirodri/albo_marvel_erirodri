9d5cff1940626d93841746b3309456461e55eb0f
Erick Rodríguez Morales <erodriguezm@intercam.com.mx>
2021-07-08 16:15:06 -0500

    Primer commit

---
+++ src/main/java/marvel/albo/erirodri/ErirodriApplication.java
@@ -0,0 +1,13 @@
+package marvel.albo.erirodri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErirodriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErirodriApplication.class, args);
+	}
+
+}
---
+++ src/test/java/marvel/albo/erirodri/ErirodriApplicationTests.java
@@ -0,0 +1,13 @@
+package marvel.albo.erirodri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErirodriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
